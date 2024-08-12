abefbcdb3546fcf9043fdcb3700bcb1d5b48b8a0
Programmer-Shivansh <shivanshchauhan2005@gmail.com>
2024-08-12 16:01:32 +0530

    [new]Base Extension

---
+++ content.js
@@ -0,0 +1,13 @@
+console.log("first")
+
+
+const comments = document.getElementById("content-text")
+console.log(comments)
+// const paragraphs = document.querySelectorAll('p');
+
+// paragraphs.forEach(paragraph => {
+
+
+//     paragraph.style.backgroundColor = 'blue';
+
+// });
